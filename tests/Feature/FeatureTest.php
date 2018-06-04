77bafa1a8fdeb42a7ae836bfa72b69fff4b87466
Leandro Tatsuya Utiyama <lutiyama@gmail.com>
2018-06-04 13:19:51 +0900

    added thread view / tests

--- tests/Feature/FeatureTest.php
+++ tests/Feature/FeatureTest.php
@@ -4,17 +4,26 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class FeatureTest extends TestCase
 {
     use DatabaseMigrations;
-    /**
-     * @test
-     */
-    public function a_user_can_browser_thread(){
-        $response->$this->get('/threads');
-        $response->assertStatus(200);
+    /** @test */
+    public function a_user_can_view_thread()
+    {
+        $thread = factory('App\Thread')->create();
+        $response = $this->get('/threads');
+        $response->assertSee($thread->title);
     }
+
+    /** @test */
+    public function a_user_can_view_single_thread()
+    {
+        $thread = factory('App\Thread')->create();
+        $response = $this->get('/threads/' . $thread->id);
+        $response->assertSee($thread->title);
+    }
+
 }
